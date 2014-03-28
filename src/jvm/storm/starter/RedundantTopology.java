0ab7005e87e42c87b592c843eaad2b461a813417
StevenYCChou <yencheng.chou@gmail.com>
2014-03-27 23:38:55 -0400

    import libraries

--- src/jvm/storm/starter/RedundantTopology.java
+++ src/jvm/storm/starter/RedundantTopology.java
@@ -1,5 +1,27 @@
 package storm.starter;
 
+
+import backtype.storm.task.TopologyContext;
+import backtype.storm.spout.SpoutOutputCollector;
+import backtype.storm.task.OutputCollector;
+import backtype.storm.topology.BasicOutputCollector;
+import backtype.storm.topology.OutputFieldsDeclarer;
+import backtype.storm.tuple.Fields;
+import backtype.storm.tuple.Tuple;
+import backtype.storm.tuple.Values;
+
+import backtype.storm.Config;
+import backtype.storm.topology.TopologyBuilder;
+import backtype.storm.topology.base.BaseRichSpout;
+import backtype.storm.topology.base.BaseBasicBolt;
+import backtype.storm.topology.base.BaseRichBolt;
+import backtype.storm.LocalCluster;
+import backtype.storm.StormSubmitter;
+
+import backtype.storm.utils.Utils;
+import java.util.Map;
+
+
 public class RedundantTopology {
 
   public static class ProfileGenerator extends BaseRichSpout {
